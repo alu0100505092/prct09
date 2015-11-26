d1b1bbdc2f5d26029aea298901783e3282f74639
alu0100505092 <alu0100505092@ull.edu.es>
2015-11-26 08:35:51 +0000

    Modificada clase lista y creados ficheros de las clases

--- lib/bibliografia.rb
+++ lib/bibliografia.rb
@@ -1,6 +1,10 @@
 require "bibliografia/version"
 require "bibliografia/lista"
 require "bibliografia/libro"
+require "bibliografia/publicaciones_periodicas"
+require "bibliografia/documento_electronico"
+require "bibliografia/articulo_periodico"
+require "bibliografia/articulo_revista"
 
 module Bibliografia
   # Your code goes here...
